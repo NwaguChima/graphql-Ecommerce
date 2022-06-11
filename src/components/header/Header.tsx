880da014caa5e8c3290406b86b2ba4db7cc2f6ce
nwagu chima <chima.nwagu@decagon.dev>
2022-06-11 16:20:26 +0100

    built header component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Logo from "../assets/Logo";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 import styles from "./Header.module.scss";
 
 interface HeaderProps {}
@@ -8,9 +9,20 @@ export const Header: React.FC<HeaderProps> = () => {
   return (
     <header className={styles.header}>
       <nav className={styles.header__nav}>
-        <a href="/products" className={styles.header__nav__link}>
+        <a href="/products" className={styles.header__nav__logo}>
           <Logo />
         </a>
+        <div className={styles.header__nav__links}>
+          <a href="/shop">Shop</a>
+          <a href="/learn">Learn</a>
+        </div>
+        <div className={styles.header__nav__profile}>
+          <a href="/account">Account</a>
+          <a href="/cart">
+            <AiOutlineShoppingCart />
+            <span>0</span>
+          </a>
+        </div>
       </nav>
     </header>
   );
