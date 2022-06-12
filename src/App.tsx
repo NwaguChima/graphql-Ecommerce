aa5293e799fa70c9377aeb5ae1f273ae8b82438c
nwagu chima <chima.nwagu@decagon.dev>
2022-06-12 07:31:26 +0100

    connected apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import Product from "./pages/Product";
 import Header from "./components/header/Header";
 import NotFound from "./pages/NotFound";
@@ -7,19 +8,40 @@ import Shop from "./pages/Shop";
 import Learn from "./pages/Learn";
 import { GlobalContextProvider } from "./context/globalContext";
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        products: {
+          merge(_, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
+const client = new ApolloClient({
+  uri: "https://pangaea-interviews.now.sh/api/graphql",
+  cache,
+});
+
 function App() {
   return (
     <>
       <GlobalContextProvider>
-        <Header />
-        <Router>
-          <Routes>
-            <Route path="/products" element={<Product />} />
-            <Route path="/shop" element={<Shop />} />
-            <Route path="/learn" element={<Learn />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Router>
+        <ApolloProvider client={client}>
+          <Header />
+          <Router>
+            <Routes>
+              <Route path="/products" element={<Product />} />
+              <Route path="/shop" element={<Shop />} />
+              <Route path="/learn" element={<Learn />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Router>
+        </ApolloProvider>
       </GlobalContextProvider>
     </>
   );
