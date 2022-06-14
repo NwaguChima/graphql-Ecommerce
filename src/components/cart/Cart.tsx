c61ad9be6f4344ea557bb1a2328fdee66282c5c0
nwagu chima <chima.nwagu@decagon.dev>
2022-06-14 01:11:06 +0100

    implemented close modal functionality for icon button

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useContext } from "react";
+import GlobalContext from "../../context/globalContext";
 import { HiOutlineChevronRight, HiOutlineChevronUp } from "react-icons/hi";
 import Button from "../button/Button";
 import styles from "./Cart.module.scss";
@@ -6,15 +7,22 @@ import styles from "./Cart.module.scss";
 interface CartProps {}
 
 const Cart: React.FC<CartProps> = () => {
+  const { setShowModal } = useContext(GlobalContext)!;
+
   return (
     <div className={styles.cart}>
       <div className={styles.cart__header}>
         <h2>My Shopping Cart</h2>
         <div className={styles.cart__header__click}>
-          <button>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              setShowModal(false);
+            }}
+          >
             <HiOutlineChevronRight />
           </button>
-          <div>
+          <div className={styles.cart__header__click__currency}>
             <label htmlFor="country">USD</label>
             <select id="country">
               <option value="usd"></option>
