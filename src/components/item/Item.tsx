75846349225693034e754cca7ee5b3c78202926e
nwagu chima <chima.nwagu@decagon.dev>
2022-06-14 02:09:36 +0100

    syled details of item component

--- src/components/item/Item.tsx
+++ src/components/item/Item.tsx
@@ -2,27 +2,28 @@ import React from "react";
 import { IoCloseOutline } from "react-icons/io5";
 import { BsPlus } from "react-icons/bs";
 import { BiMinus } from "react-icons/bi";
+import styles from "./Item.module.scss";
 
 interface ItemProps {}
 
 const Item: React.FC<ItemProps> = () => {
   return (
-    <div>
-      <div className="head">
-        <h5>Classic Maintenance Set</h5>
+    <div className={styles.item}>
+      <div className={styles.item__head}>
+        <h4>Classic Maintenance Set</h4>
         <i>
           <IoCloseOutline />
         </i>
       </div>
       <p>Oily / Two Months</p>
-      <div className="delivery">
+      <div className={styles.item__delivery}>
         <p>Two months supply shipped every two months</p>
         <img
           src="https://d1b929y2mmls08.cloudfront.net/luminskin/img/new-landing-page/moisturizing-balm.png"
           alt="item badge"
         />
       </div>
-      <div className="details">
+      <div className={styles.item__details}>
         <button>
           <i>
             <BiMinus />
