53c4a3707ca0555839b845eae77bb0749bb7a073
nwagu chima <chima.nwagu@decagon.dev>
2022-06-14 03:31:53 +0100

    implemented cart count

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,25 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import Logo from "../assets/Logo";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import GlobalContext from "../../context/globalContext";
 import styles from "./Header.module.scss";
+import { ProductType } from "../../utils/types";
 
 interface HeaderProps {}
 
 export const Header: React.FC<HeaderProps> = () => {
   const { setShowModal, setClose, cart } = useContext(GlobalContext)!;
+  const [quantity, setQuantity] = useState(0);
+
+  useEffect(() => {
+    let arr = [...cart];
+    const total =
+      cart.length > 1
+        ? arr.reduce((acc: any, item: any) => acc + item.quantity, 0)
+        : 0;
+
+    setQuantity(total);
+  }, [cart]);
 
   const handleClick = () => {
     setShowModal(true);
@@ -41,7 +53,7 @@ export const Header: React.FC<HeaderProps> = () => {
             <i>
               <AiOutlineShoppingCart />
             </i>
-            <span>{cart.length}</span>
+            <span>{quantity}</span>
           </div>
         </div>
       </nav>
